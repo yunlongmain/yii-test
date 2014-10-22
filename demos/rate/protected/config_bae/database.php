82411c339d94a4c693736448d9f44671e0957a60
yunlong <yunlong@baidu.com>
2014-10-22 17:05:37 +0800

    bae

--- demos/rate/protected/config_bae/database.php
+++ demos/rate/protected/config_bae/database.php
@@ -4,7 +4,7 @@
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	'connectionString' => 'mysql:host=sqld.duapp.com:4050;dbname=eOUgROBQMLEYpaGRKRnF',
+	'connectionString' => 'mysql:host=sqld.duapp.com;port=4050;dbname=eOUgROBQMLEYpaGRKRnF',
     'emulatePrepare' => true,
     'username' => 'orjQWFecVu9hzkzSjEQOOQSt',
     'password' => 'VbgymkVrmzEELiEG6opSXGwpuz1BYCo5',
